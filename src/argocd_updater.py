1de14848dbf6f37015c6a5852b6b88b8ef86c4d0
Oguzhan Yilmaz <oguzhan@hepapi.com>
2025-03-08 12:43:04 +0300

    log level info

--- src/argocd_updater.py
+++ src/argocd_updater.py
@@ -111,9 +111,9 @@ def update_app_spec_with_new_hpa_config(app_name, app_spec: Dict, new_hpa_config
     # otherwise ArgoCD API will error: ''spec is invalid: application destination can't have both name and server defined''
     if 'server' in app_spec['destination']:
         if 'name' in app_spec['destination']:
-            logger.debug(f'ArgoApp({app_name}) .spec.destination has .server and .name defined in it. Removing .server definition.')
+            logger.info(f'ArgoApp({app_name}) .spec.destination has .server and .name defined in it. Removing .server definition.')
             app_spec['destination'].pop('server')
-            logger.debug(f"ArgoApp({app_name}) Updated .destination: {app_spec['destination']}")
+            logger.info(f"ArgoApp({app_name}) Updated .destination: {app_spec['destination']}")
 
     return app_spec
 
